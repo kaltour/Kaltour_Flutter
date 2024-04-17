50f5bf77473731af589943c4252ce138791e88e5
dawoon <dwjeong@kaltour.com>
2024-04-17 14:14:46 +0900

    백그라운드 메시지 데이터 처리 및 기존 페이지 주석처리

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,18 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   print("백그라운드 메시지 처리.. ${message.notification!.body!}");
   print("백그라운드 데이터 처리 ${message.data}");
 }
+
 @pragma('vm:entry-point')
 void backgroundHandler(NotificationResponse details) {
-
 }
 
-void main()  async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   print("!!!RUN APP!!!");
 
   await Firebase.initializeApp();
   initializeNotification();
-  initNoti();
+  WidgetsFlutterBinding.ensureInitialized();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   FirebaseMessaging.instance.requestPermission(
     badge: true,
@@ -35,51 +35,64 @@ void main()  async {
   FirebaseMessaging.instance.onTokenRefresh.listen((newToken) async {
     // save token to server
   });
-
   runApp(MyApp());
-
 }
-class MyApp extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter WebView Sample',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: WebViewExample(),
+void _handleMessageOpenedApp(RemoteMessage message, BuildContext context) {
+  print("오픈 메시지");
+  String url = message.data['sequence'];
+  if (url != null) {
+    print("시퀀스가 있음");
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MyWebViewExample(url)),
     );
   }
 }
 
+void _configureFirebaseMessaging(BuildContext context) {
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    _handleMessageOpenedApp(message, context);
+  });
+}
 
-// void initializeNotification() async {
-//   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-//   final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-//   await flutterLocalNotificationsPlugin
-//       .resolvePlatformSpecificImplementation<
-//       AndroidFlutterLocalNotificationsPlugin>()
-//       ?.createNotificationChannel(const AndroidNotificationChannel(
-//
-//       'high_importance_channel', 'high_importance_notification',
-//       importance: Importance.max));
-//
-//   await flutterLocalNotificationsPlugin.initialize(const InitializationSettings(
-//
-//     android: AndroidInitializationSettings("@mipmap/ic_launcher"),
-//
-//
-//   ));
-//
-//   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-//     alert: true,
-//     badge: true,
-//     sound: true,
-//   );
-//
-// }
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    _configureFirebaseMessaging(context);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: WebView(
+        initialUrl: "https://m.kaltour.com/",
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+
+    // return Scaffold(
+    //   body: SafeArea(
+    //     child: WebView(
+    //       initialUrl: "https://m.kaltour.com/",
+    //       javascriptMode: JavascriptMode.unrestricted,
+    //     ),
+    //   ),
+    // );
+  }
+}
+
 
 void initializeNotification() async {
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
@@ -99,109 +112,132 @@ void initializeNotification() async {
     alert: true,
     badge: true,
     sound: true,
-
   );
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-    print("오픈 메시지 데이터 처리 ${message.data}");
-  });
+  // FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+  //   print("오픈 메시지 데이터 처리 ${message.data}");
+  //   String url = message.data['sequence'];
+  //   if(url != null) {
+  //     Navigator.push(
+  //
+  //     );
+  //   }
+  // });
+
   RemoteMessage? message = await FirebaseMessaging.instance.getInitialMessage();
-
   if (message != null) {
-
   }
 }
 
-void initNoti() async {
 
+class MyWebViewExample extends StatelessWidget {
+  // const MyWebViewExample({super.key});
 
-}
+  final String url;
 
-
-
-
-class WebViewExample extends StatefulWidget {
-  @override
-  _WebViewExampleState createState() => _WebViewExampleState();
-
-
-}
-class _WebViewExampleState extends State<WebViewExample> {
-  var messageString = "";
-  void getMyDeviceToken() async {
-
-    final token = await FirebaseMessaging.instance.getToken();
-    print("내 디바이스 토큰: $token");
-  }
-
-  final dio = Dio();
-  void request() async {
-    Response response;
-    final myToken = FirebaseMessaging.instance.getToken();
-    print("나의 토큰: $myToken");
-    const token = "X%2FWnoeM%2BhLdu9VP7ncdF5A%3D%3D";
-    // The below request is the same as above.
-    response = await dio.get(
-      'https://www.kaltour.com/API/WebPush/call',
-      queryParameters: {
-        "TOK": myToken,
-        "TYP": "M",
-        "GNT": "",
-        "CID": "",
-        "URL": "gohanway.kaltour.com",
-        "PTH": "IOS",
-        "AK" : "X%2FWnoeM%2BhLdu9VP7ncdF5A%3D%3D"
-      },
-    );
-    print(response.data.toString());
-  }
+  MyWebViewExample(this.url);
 
   @override
-
-  void initState() {
-    getMyDeviceToken();
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) async { // FCM 수신
-      RemoteNotification? notification = message.notification;
-      if (notification != null) {
-        FlutterLocalNotificationsPlugin().show(
-          notification.hashCode,
-          notification.title,
-          notification.body,
-          const NotificationDetails(
-            android: AndroidNotificationDetails(
-              'high_importance_channel',
-              'high_importance_notifications',
-              importance: Importance.max,
-            ),
-          ),
-        );
-        setState(() {
-          messageString = message.notification!.body!;
-          print("Foreground 메시지 수신: $messageString");
-        });
-      }
-
-    });
-
-    super.initState();
-
-  }
-
   Widget build(BuildContext context) {
-
-    var realUrl = "https://m.kaltour.com/";
-
     return Scaffold(
-      // appBar: AppBar(
-      //   // title: Text('WebView Example'),
+      // body: WebView(
+      //   initialUrl: url,
+      //   javascriptMode: JavascriptMode.unrestricted,
       // ),
-      body: SafeArea (
-        child: WebView (
-          initialUrl: realUrl,
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
-
-      )
-
+      body: Center(
+        child: Text("hi"),
+      ),
     );
   }
 }
+
+//
+// class WebViewExample extends StatefulWidget {
+//
+//
+//
+//   @override
+//   _WebViewExampleState createState() => _WebViewExampleState();
+//
+//
+// }
+// class _WebViewExampleState extends State<WebViewExample> {
+//   var messageString = "";
+//
+//   void getMyDeviceToken() async {
+//
+//     final token = await FirebaseMessaging.instance.getToken();
+//     print("내 디바이스 토큰: $token");
+//   }
+//
+//   final dio = Dio();
+//   void request() async {
+//     Response response;
+//     final myToken = FirebaseMessaging.instance.getToken();
+//     print("나의 토큰: $myToken");
+//     const token = "X%2FWnoeM%2BhLdu9VP7ncdF5A%3D%3D";
+//     // The below request is the same as above.
+//     response = await dio.get(
+//       'https://www.kaltour.com/API/WebPush/call',
+//       queryParameters: {
+//         "TOK": myToken,
+//         "TYP": "M",
+//         "GNT": "",
+//         "CID": "",
+//         "URL": "gohanway.kaltour.com",
+//         "PTH": "IOS",
+//         "AK" : "X%2FWnoeM%2BhLdu9VP7ncdF5A%3D%3D"
+//       },
+//     );
+//     print(response.data.toString());
+//   }
+//
+//   @override
+//
+//   void initState() {
+//     getMyDeviceToken();
+//     FirebaseMessaging.onMessage.listen((RemoteMessage message) async { // FCM 수신
+//       RemoteNotification? notification = message.notification;
+//       if (notification != null) {
+//         FlutterLocalNotificationsPlugin().show(
+//           notification.hashCode,
+//           notification.title,
+//           notification.body,
+//           const NotificationDetails(
+//             android: AndroidNotificationDetails(
+//               'high_importance_channel',
+//               'high_importance_notifications',
+//               importance: Importance.max,
+//             ),
+//           ),
+//         );
+//         setState(() {
+//           messageString = message.notification!.body!;
+//           print("Foreground 메시지 수신: $messageString");
+//         });
+//       }
+//
+//     });
+//
+//     super.initState();
+//
+//   }
+//
+//   Widget build(BuildContext context) {
+//
+//     var realUrl = "https://m.kaltour.com/";
+//
+//     return Scaffold(
+//       // appBar: AppBar(
+//       //   // title: Text('WebView Example'),
+//       // ),
+//       body: SafeArea (
+//         child: WebView (
+//           initialUrl: realUrl,
+//           javascriptMode: JavascriptMode.unrestricted,
+//         ),
+//
+//       )
+//
+//     );
+//   }
+// }
